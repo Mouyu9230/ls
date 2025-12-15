41268dfad50002b650d140944554bf44811e9269
Mouyu9230 <2313228276@qq.com>
2025-12-15 19:39:00 +0800

    big progress

--- ls.c
+++ ls.c
@@ -1,8 +1,8 @@
 //用不同的函数块实现不同的需求,为确保可以组合使用参数，考虑通过在list中增加其他函数是否执行的判定来完成组
 //合，而不是分别在各个参数对应的函数块完整的实现对应的目标
 //---------------------------------------------------------------
-//-a: 显示隐藏文件 决定list在循环获取文件名输出时是否取消检测首字符的环节 已完成
-//-l：显示详细信息 用stat获取详细信息，在输出文件名的循环前加入判定，若有则输出stat的信息
+//-a: 显示隐藏文件 
+//-l：显示详细信息 
 //-R：递归显示
 //-t：按时间显示，最新最靠前
 //-r：对目录反向排序（以目录的每个首字符所对应的ASCII值进行大到小排序）
@@ -15,25 +15,33 @@
 #include <unistd.h>//stat,getcwd
 #include<stdlib.h>//exit...?
 #include<string.h>
-#include<fcntl.h>
+#include<pwd.h>//getpwduid与返回结构体
+#include<grp.h>//getgrgid与返回结构体
+#include<time.h>//ctime函数
+
 
 #define purple "\033[35m"//为后面分颜色输出用宏定义包装对应颜色的代码
 #define blue "\033[34m"
+#define green "\033[32m"
+#define yellow "\033[33m"
 #define red "\033[31m"
 #define cyan "\033[36m"
 #define reset "\033[0m"
 
 int aflag,lflag,Rflag,tflag,rflag,iflag,sflag;
 
+int dcheck(struct stat stats){
+    if(S_ISREG(stats.st_mode)!=0){
+        return 0;
+    }
+    return 1;
+}
 
-
-void information(char* filename,char* pathname){
-    char wholepath[9999];
-    struct stat stats;
-    snprintf(wholepath,sizeof(wholepath),"%s/%s",pathname,filename);
-    stat(wholepath,&stats);
+void showinformation(struct stat stats){
     if(S_ISREG(stats.st_mode)!=0){//文件类型（未完成
         printf("-");
+    }else{
+        printf("d");
     }
     if((stats.st_mode&S_IRUSR)!=0)printf("r");else{printf("-");}//权限
     if((stats.st_mode&S_IWUSR)!=0)printf("w");else{printf("-");}
@@ -44,14 +52,12 @@ void information(char* filename,char* pathname){
     if((stats.st_mode&S_IROTH)!=0)printf("r");else{printf("-");}
     if((stats.st_mode&S_IWOTH)!=0)printf("w");else{printf("-");}
     if((stats.st_mode&S_IXOTH)!=0)printf("x ");else{printf("- ");}
-    printf("%d ",stats.st_nlink);//（未完成
-    printf("%s %s ",stats.st_uid,stats.st_gid);
-    printf("%lld",stats.st_size);
-    printf("%s ",stats.st_mtime);
-
-
- 
-
+    printf("%d ",(int)stats.st_nlink);
+    struct passwd *pw = getpwuid(stats.st_uid);
+    struct group  *gr = getgrgid(stats.st_gid);
+    printf("%s %s ",pw->pw_name,gr->gr_name);
+    printf("%lld ",(long long int)stats.st_size);
+    printf("%s",ctime(&stats.st_mtime));
 }
 
 void list(int aflag,int lflag,int Rflag,int tflag,int rflag,int iflag,int sflag,char* path){
@@ -76,11 +82,29 @@ void list(int aflag,int lflag,int Rflag,int tflag,int rflag,int iflag,int sflag,
         if(content==NULL){
             break;
         }
+    char wholepath[9999];
+    struct stat stats;
+    snprintf(wholepath,sizeof(wholepath),"%s/%s",pathname,content->d_name);//拼接
+    stat(wholepath,&stats);
         if(content->d_name[0]!='.'||aflag==1){//-a判定
-            if(lflag==1){
-                information(content->d_name,pathname);
+            if(lflag==1){//-l判定
+                showinformation(stats);
+            }//根据各种条件分颜色输出
+            if(iflag==1){
+                printf(blue"%lld "reset,(long long)stats.st_ino);
             }
-            printf("%s\n",content->d_name);
+            if(sflag==1){
+                printf(purple"%lld "reset,(long long)stats.st_blocks);
+            }
+            if(dcheck(stats)){
+                printf(red"%s\n"reset,content->d_name);
+            }else if(content->d_name[0]=='.'){
+                printf(yellow"%s\n"reset,content->d_name);
+            }else{
+                printf(cyan"%s\n"reset,content->d_name);
+            }
+
+                
         }
 
     }
@@ -125,7 +149,7 @@ int main(int argc,char* argv[]){
         }
 
     }
-    if((extrastrcount!=0&&argc==2)||argc==1){//没输入路径就getcwd输出当前目录内容
+    if(extrastrcount==argc-1||argc==1){//没输入路径就getcwd输出当前目录内容
         list(aflag,lflag,Rflag,tflag,rflag,iflag,sflag,NULL);
     }else{//有的话就遍历argv输出，跳过参数字符串
          for(int i=1;i<argc;i++){
